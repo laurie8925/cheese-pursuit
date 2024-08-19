3644750e721f872b25cd862502d359f71a7eac7e
Laurie Yeh <laurie8925@gmail.com>
2024-08-18 21:31:29 -0700

    added volume mute button and cahgen svg

--- dist/index.js
+++ dist/index.js
@@ -95,18 +95,60 @@ const player = new Audio("medias/Baby-Tiger.mp3"); // Ensure this path is correc
 const volumeSlider = document.getElementById("music-control");
 const volumeDisplay = document.getElementById("volume-display"); // Optional
 const playPauseButton = document.getElementById("play");
+// const pauseButton = document.getElementById("pause") as HTMLInputElement;
 if (!player || !volumeSlider || !playPauseButton) {
     console.error("One or more elements not found.");
 }
-player.pause();
+player.play();
 playPauseButton.addEventListener("click", () => {
-    if (!player.paused) {
-        player.pause();
-        playPauseButton.textContent = "Play";
+    if (player.paused) {
+        player.play();
+        playPauseButton.innerHTML = `<svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-volume"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#ffcd28"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M15 8a5 5 0 0 1 0 8" />
+                <path d="M17.7 5a9 9 0 0 1 0 14" />
+                <path
+                  d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5"
+                />
+              </svg>`;
     }
     else {
-        player.play();
-        playPauseButton.textContent = "Pause";
+        player.pause();
+        playPauseButton.innerHTML = `<svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-volume-off"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#ffcd28"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path
+                  d="M15 8a5 5 0 0 1 1.912 4.934m-1.377 2.602a5 5 0 0 1 -.535 .464"
+                />
+                <path
+                  d="M17.7 5a9 9 0 0 1 2.362 11.086m-1.676 2.299a9 9 0 0 1 -.686 .615"
+                />
+                <path
+                  d="M9.069 5.054l.431 -.554a.8 .8 0 0 1 1.5 .5v2m0 4v8a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l1.294 -1.664"
+                />
+                <path d="M3 3l18 18" />
+              </svg>`;
     }
 });
 // Optional: Add event listener for volume slider
